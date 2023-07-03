9ceafa86f97389bd68b01be27b175d8237c9ae61
simeon1534 <kolevsimeon64@gmail.com>
2023-07-03 18:13:34 +0300

    New function for adding

--- towns.js
+++ towns.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+	$('#btnDelete').click(deleteTown);
+	$('#btnAdd').click(addTown);
 });
 
 function deleteTown() {
@@ -17,3 +18,10 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+
+function addTown() {
+	let townName = $('#townNameForAdd').val();
+	$('#townNameForAdd').val('');
+	$('#towns').append($('<option>').text(townName));
+	$('#result').text(townName + " added.");
+	}
