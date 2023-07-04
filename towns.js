1b2a256fe2d1036d7455d653f5115a7c49edf830
simeon1534 <kolevsimeon64@gmail.com>
2023-07-04 18:55:02 +0300

    delete func new

--- towns.js
+++ towns.js
@@ -7,22 +7,28 @@ $(document).ready(function() {
 	$('#btnAdd').click(addTown);
 });
 
+
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
 	let removed = false;
 	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
+	if (option.textContent == townName) {
+	removed = true;
+	option.remove();
+	}
 	}
 	if (removed)
-		$('#result').text(townName + " deleted.");
+	showMessage(townName + " deleted.");
 	else
-		$('#result').text(townName + " not found.");
-}
-
+	showMessage(townName + " not found.");
+	}
+	function showMessage(msg) {
+	$('#result').text(msg).css("display", "block");
+	setTimeout(function () {
+	$('#result').hide('blind', {}, 500);
+	}, 3000);
+	}
 function shuffleTowns() {
 	let towns = $('#towns option').toArray();
 	$('#towns').empty();
